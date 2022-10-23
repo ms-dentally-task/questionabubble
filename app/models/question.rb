1ef865e480c29f2d85dffbd1ffef5ccc18ff11d4
Max Shelley <mail@maxshelley.com>
2022-10-23 17:30:23 +0100

    Generate slug when a Question is saved
    We want to be able to reference a question with a non-sequential ID so
    we can assign it to a user after they register.
    
    A trade-off for time here is that we're not rescuing the exception if
    the generated slug were to clash with an existing one, an exception
    would be raised by the unique index on the column.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
+  before_validation :create_slug, if: -> { slug.blank? }
+
   belongs_to :user, optional: true
 
   validates_presence_of :body, message: 'Please enter some text for your question.'
   validates :slug, presence: true
+
+  private
+
+  def create_slug
+    self.slug = SecureRandom.hex(5)
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,6 +5,23 @@ require 'rails_helper'
 describe Question, type: :model do
   it { is_expected.to belong_to(:user).optional }
 
-  it { is_expected.to validate_presence_of(:slug) }
   it { is_expected.to validate_presence_of(:body).with_message(/Please enter some text/) }
+
+  describe '#create_slug' do
+    it 'creates a slug before saving' do
+      question = Question.new(body: 'DummyBody')
+
+      question.save!
+
+      expect(question.slug).to_not be_nil
+    end
+
+    it 'does not change the slug when updated' do
+      question = Question.create!(body: 'DummyBody', slug: 'example-slug')
+
+      question.update!(body: 'UpdatedBody')
+
+      expect(question.slug).to eq 'example-slug'
+    end
+  end
 end
